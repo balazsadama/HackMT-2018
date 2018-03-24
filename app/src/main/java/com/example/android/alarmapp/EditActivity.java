f9563965f54b358feb909d04589ccb4fd5c6664e
balazsadama <balazs.adam.a@gmail.com>
2018-03-24 18:47:41 +0200

    edit oldal nagyjabol kesz

---
+++ app/src/main/java/com/example/android/alarmapp/EditActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.alarmapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Adam on 3/24/2018.
+ */
+
+public class EditActivity extends AppCompatActivity {
+}
